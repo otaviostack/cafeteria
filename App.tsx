c86d9c12b3da3e7239ddd078ab60e65445bd3515
otaviostack <otavioastoniolo73@gmail.com>
2026-09-09 15:18:23 -0300

    adicionando imagem no projeto e definindo textos

--- App.tsx
+++ App.tsx
@@ -1,25 +1,46 @@
 import { Ionicons } from '@expo/vector-icons';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <ScrollView style={style.container}>
+    <ScrollView style={styles.container}>
       {/* Header */}
-      <View style={style.header}>
+      <View style={styles.header}>
         <View>
-          <Text>Café do Código</Text>
-          <Text>Seu café, uma linha por vez</Text>
+          <Text style={styles.headerTitle}>Café do Código</Text>
+          <Text style={styles.headerSubtitle}>Seu café, uma linha por vez</Text>
         </View>
-        <View>
-         <Ionicons name='person' size={20} color="#2f2d2c"></Ionicons>
+        <View style={styles.avatarPlaceHolder}>
+          <Ionicons name='person' size={20} color="#2f2d2c"></Ionicons>
+        </View>
+      </View>
+
+      {/* Conteudo */}
+      <View style={styles.content}>
+        <View style={styles.greetingSection}>
+          <Text style={styles.greetingTitle}>Bom dia!</Text>
+          <Text style={styles.greetingSubtitle}>Que tal um café hoje?</Text>
         </View>
 
+
+
+        <View>
+          <Image source={require('./assets/coffee.jpg')}></Image>
+          <Text>Cappucino Especial</Text>
+          <Text>Cremoso e delicioso</Text>
+          <Text>R$ 12,90</Text>
+        </View>
       </View>
+
+
+
+     
+
     </ScrollView>
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f9f9f9'
@@ -29,8 +50,45 @@ const style = StyleSheet.create({
     paddingHorizontal: 24,
     paddingTop: 67,
     paddingBottom: 20,
-    flexDirection:"row",
-    justifyContent:"space-between",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center"
+  },
+  headerTitle: {
+    fontSize: 22,
+    fontWeight: "800",
+    color: "#2f2d2c"
+  },
+  headerSubtitle: {
+    fontSize: 14,
+    color: "#9b9b9b",
+    marginTop: 4
+  },
+  avatarPlaceHolder: {
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  content: {
+    paddingHorizontal: 24,
+
+  },
+  greetingSection: {
+    marginTop: 10,
+    marginBottom: 24,
+  },
+  greetingTitle: {
+    fontSize: 32,
+    fontWeight: "800",
+    color: "#2f2d2c",
+  },
+  greetingSubtitle: {
+    fontSize: 16,
+    color: "#9b9b9b",
+    marginTop: 8,
   }
+
 })
